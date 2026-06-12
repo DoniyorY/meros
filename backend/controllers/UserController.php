a7d502354fb82bf5e197bc833a4a1403fa8c2665
Victus Ubuntu <yusupovdoniyor54@gmail.com>
2026-06-12 17:26:05 +0500

    123

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -52,9 +52,9 @@ class UserController extends Controller
       ]);
    }
    
-   public function actionSendEmail()
+   public function actionSendEmail($id)
    {
-      $user = User::findOne(['id'=>4]);
+      $user = User::findOne(['id'=>$id]);
       echo "<pre>";
       var_dump($user->sendEmail($user));
    }
