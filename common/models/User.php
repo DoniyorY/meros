51dfa273730402a2ec8e314919f8c2fdc5ed4255
Victus Ubuntu <yusupovdoniyor54@gmail.com>
2026-07-03 16:21:54 +0500

    user email token fix

--- common/models/User.php
+++ common/models/User.php
@@ -121,7 +121,6 @@ class User extends ActiveRecord implements IdentityInterface
     public static function findByVerificationToken($token) {
         return static::findOne([
             'verification_token' => $token,
-            'status' => self::STATUS_INACTIVE
         ]);
     }
 
